d1feda5a0266e3b65b68b6b9b4312f37186669ca
pierfrancescoscar <scarinci401@gmail.com>
2021-11-04 13:15:17 +0100

    add general setup

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Our Team
+// - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
